6975ec2551a51c062232527c4269383de0739f96
Ricardo Ferruz <ricardoferruz@MacBook-Air-de-Ricardo.local>
2025-12-13 09:33:46 +0100

    feat: add sitemap and robots.txt generation

---
+++ next-sitemap.config.js
@@ -0,0 +1,19 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://ridatours.com',
+  generateRobotsTxt: true,
+  sitemapSize: 7000,
+  changefreq: 'daily',
+  priority: 0.7,
+  exclude: ['/admin/*', '/api/*'],
+  robotsTxtOptions: {
+    additionalSitemaps: [],
+    policies: [
+      {
+        userAgent: '*',
+        allow: '/',
+        disallow: ['/admin', '/api'],
+      },
+    ],
+  },
+}
